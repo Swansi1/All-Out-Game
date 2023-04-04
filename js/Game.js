23a2d789842c34682ecbf76b15fc696400be3b71
Swansi <patrikzrinyi404@gmail.com>
2023-04-04 16:40:26 +0200

    scoreboard GUI implementation + bootstrap

--- js/Game.js
+++ js/Game.js
@@ -45,24 +45,31 @@ let palyak = [
 
 
 class Game{
-    constructor(difficulty=1){
-        this._difficulty = difficulty;
+    constructor(){
         this._GameSize = [5,5]; // 5x5 pálya
         // 2d array amiben tároljuk a pálya nagyságát és az értékeket
-        this._GameTable = this._initGameTable();
+        // this._GameTable = this._initGameTable();
         
         this._currentTable = 1;
+        this.changeGameTable(this._currentTable);
     }
 
     // megváltoztatja a pályát az aktuális kijelöltre
-    changeGameTable(tableId){
-        this._currentTable =  palyak[tableId];
+    changeGameTable(tableId=-1){
+        if(tableId == -1){
+            tableId = this._currentTable;
+        }
+        this._GameTable =  palyak[tableId];
         this._initGameTable();
     }
 
     // vissza adja a pályák számát
     get getMaxTableNumber(){
         return palyak.length;
+    }
+
+    getCurrentCell(x,y){
+        return this._GameTable[y][x];
     }
 
     // ez fogja léptetni a következőre 
@@ -119,7 +126,6 @@ class Game{
         // [x,y,érték]
         let szomszedok = {"fent": [], "lent": [], "balra": [], "jobbra": []}; // üres array ha nincs szomszéd(pályán kívül kerül)
 
-
         let newY = y + 1
         if(newY < this._GameSize[0]){ // fent
             szomszedok.fent = [x, newY, this._GameTable[newY][x]]; 
@@ -161,19 +167,39 @@ class Game{
 
 class Scoreboard{
     constructor() {
-        let scoreboard = JSON.parse(window.localStorage.getItem('scoreboard'));
-        if(scoreboard == null){
+        this._currentName = name; 
+        this.scoreboard = [];
+        let scores = JSON.parse(window.localStorage.getItem('scoreboard'));
+        if(scores == null){
             let newScore = [];  
-            this.scoreboard = newScore;
+            this._scoreboardAll = newScore;
             window.localStorage.setItem("scoreboard",JSON.stringify(newScore));
         }else{
-            this.scoreboard = scoreboard; // array [["nev", lepes, timestamp]];
+            this._scoreboardAll = scores; // array [["nev", lepes, timestamp]];
         }
+    }
+
+    set setName(name){
+        // játékos nevének beállítása
+        this._currentName = name;
+
+        let sorted = []; // 
+        for (let i = 0; i < this._scoreboardAll.length; i++) {
+            const element = this._scoreboardAll[i];
+            if(element[0] === name){
+                sorted.push(element);
+            }
+        }
+        this.scoreboard = sorted;
     }
 
     set setScore(score){ // score: array["név", lepesek, timestamp]
         this.scoreboard.push(score);
         window.localStorage.setItem("scoreboard", JSON.stringify(this.scoreboard));
+    }
+
+    get getName(){
+        return this.name;
     }
 
     get getScoreboard(){
@@ -183,5 +209,122 @@ class Scoreboard{
     clearScoreboard(){
         this.scoreboard = [];
         window.localStorage.setItem("scoreboard", JSON.stringify(this.scoreboard));
+    }
+} 
+
+
+
+
+// GUI onclick methods
+
+$("#gameStartBtn").click(function (e) { 
+    e.preventDefault();
+    let nev = $("#nevFormInput").val();
+    console.log(nev);
+    gui.scoreboard.setName = nev; // játékos név beállítsáa
+    $("#usernameWelcome").html(nev); // név beállítása
+    gui.game.changeGameTable() // azért kell hogy nulláza a timert
+
+    // rejtsük el a login felületet majd jelenjen meg a játéktér
+   $("#loginDiv").fadeOut(2000, function(){
+       $(this).addClass("d-none"); // bootstrap miatt
+       $("#gameDiv").removeClass("d-none");
+       $("#gameDiv").fadeIn(2000);
+       gui.createTable();
+       gui.setScoreboard();
+   });
+});
+
+$(".col-md").hover(function () {
+    $(this).toggleClass("bg-primary");
+ });
+
+
+// Controller
+let gui = {};
+gui.scoreboard = new Scoreboard();
+gui.game = new Game();
+
+gui.scoreboard.getTimeFromTimestamp = function(timestamp){
+    // Az aktuális idő lekérése
+    const currentDate = new Date();
+        
+    // Az eltelt idő kiszámítása milliszekundumban
+    const elapsedTime = currentDate.getTime() - new Date(timestamp).getTime();
+    
+    // Az eltelt idő átalakítása  óra, perc, másodperc formátumra
+    const hours = Math.floor((elapsedTime / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    const seconds = Math.floor((elapsedTime / 1000) % 60);
+
+    return [hours,minutes,seconds];
+}
+
+gui.setScoreboard = function (){
+    // jelenlegi scoreok:
+
+    // Frissíti az időt az aktuális időre minden másodpercben
+    setInterval(function() {
+    
+        //TODO nem lesz jó mert nem kell kivonni a dátumból csak  az idők kellenek a timestampból
+        // TODO vagy úgy mented el, hogy start és stop timestamp
+    const [hours, minutes, seconds] = gui.scoreboard.getTimeFromTimestamp(gui.game._startTime);
+    
+    // Az eredmény frissítése a HTML-ben
+    if(minutes == 0){
+        $("#idoScore").html( seconds + " másodperc");
+    }else{
+        if(hours != 0){
+            $("#idoScore").html(hours + " óra, " + minutes + " perc, " + seconds + " másodperc");
+        }else{
+            $("#idoScore").html( minutes + " perc, " + seconds + " másodperc");
+        }
+    }
+    }, 1000);
+
+    // $("#idoScore").html(gui.game.moveCount);
+    $("#moveScore").html(gui.game.moveCount);
+    
+
+    let currentScore = gui.scoreboard.getScoreboard;
+    for (let i = 0; i < currentScore.length; i++) {
+        const score = currentScore[i];
+        const [hours, minutes, seconds] = gui.scoreboard.getTimeFromTimestamp(score[2]);
+        let stringTime = "";
+        if(minutes == 0){
+            stringTime = seconds + " másodperc";
+        }else{
+            if(hours != 0){
+                stringTime = hours + " óra, " + minutes + " perc, " + seconds + " másodperc";
+            }else{
+                stringTime = minutes + " perc, " + seconds + " másodperc";
+            }
+        }
+        $("#scoreAppend").append(`<div class="row"><div class="col">${score[1]}</div><div class="col">${stringTime}</div></div>`);
+    }
+    if(currentScore.length === 0){
+        // ha nincs score akkor írjuk ki, hogy nincs még végzett játék
+        $("#scoreAppend").html(`<div class="col">Nincsen még elmentett játék :(</div>`);
+    }
+    // $("#scoreAppend")
+}
+
+gui.createTable = function () {
+    for (let i = 0; i < 5; i++) {
+        let row = $(`<div class="row"></div>`);
+        for (let x = 0; x < 5; x++) {
+            let selected = gui.game.getCurrentCell(x,i) == 1 ? 'bg-primary' : '';
+            let col = $(`<div id="col${i}${x}" data-x="${x}" data-y="${i}" class="col-md border ${selected} rounded m-2"></div>`);
+            // onclick
+            $(col).click(function (){
+                console.log(this);
+            });
+
+            $(col).hover(function(){
+                $(this).toggleClass("bg-primary");
+            });
+            $(row).append(col);
+        }
+        $("#gameTable").append(row);
     }
 }
